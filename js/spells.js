b699773d5e647245dfe7c85d93f327a31821d4ee
Vsevolod Kvachev <rasielll@gmail.com>
2016-04-10 00:20:38 +0300

    started to work on cooldowns

--- js/spells.js
+++ js/spells.js
@@ -1,16 +1,16 @@
 Spell = function() {
     this.cooldown = 0
     this.currentCooldown = 0
-    
-
 };
-
 
 Spell.prototype.cast = function(character) {
     this.currentCooldown = this.cooldown
     console.log("Casting spell")
 }
 
+Spell.prototype.onCooldown = function(character) {
+    game.time.now
+}
 
 
 function HealingSpell() {
@@ -22,5 +22,7 @@ HealingSpell.prototype = Object.create(Spell.prototype);
 HealingSpell.prototype.constructor = HealingSpell
 
 HealingSpell.prototype.cast = function(character){
+    this.currentCooldown = this.cooldown
+
     character.health = character.health + healingSpellHealingPercentage
 };
