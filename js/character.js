ba544d5784ab33c9986d6a05a28e3d03089c3841
Vsevolod Kvachev <rasielll@gmail.com>
2016-04-10 00:53:10 +0300

    placeholders for all remaining spells

--- js/character.js
+++ js/character.js
@@ -106,10 +106,17 @@ Character = function (index, game, x, y, r, g, b) {
     this.shouldCastSpell1 = false
     this.shouldCastSpell2 = false
     this.shouldCastSpell3 = false
+    this.shouldCastSpell4 = false
+    this.shouldCastSpell5 = false
     this.touchInputChanged = false
 
     this.spell0Slot = new Spell()
     this.spell1Slot = new HealingSpell()
+    this.spell2Slot = new Leap()
+    this.spell3Slot = new Spike()
+    this.spell4Slot = new ColdSphere()
+    this.spell5Slot = new Poison()
+
     this.recolorAura()
 
     this.hpBar = null;
@@ -240,6 +247,14 @@ Character.prototype.update = function() {
     {
         this.shouldCastSpell3 = true
     }
+    if (this.cursor.spell4)
+    {
+        this.shouldCastSpell4 = true
+    }
+    if (this.cursor.spell5)
+    {
+        this.shouldCastSpell5 = true
+    }
 
     // commit movement
     if (this.shouldMoveLeft) {
@@ -290,6 +305,14 @@ Character.prototype.update = function() {
     if (this.shouldCastSpell3)
     {
         this.shouldCastSpell3 = false
+    }
+    if (this.shouldCastSpell4)
+    {
+        this.shouldCastSpell4 = false
+    }
+    if (this.shouldCastSpell5)
+    {
+        this.shouldCastSpell5 = false
     }
 
     this.headSprite.x = this.baseSprite.x;
