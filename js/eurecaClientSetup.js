700b282d4c95eb49f6b20751efa127207c7a3fa9
Atomofiron <atomofiron@yandex.ru>
2016-04-10 03:23:36 +0300

    start at 3 attack

--- js/eurecaClientSetup.js
+++ js/eurecaClientSetup.js
@@ -44,12 +44,10 @@ var EurecaClientSetup = function() {
 		}
 	}
 
-	eurecaClient.exports.castRemoteAttack = function(id, target)
+	eurecaClient.exports.castRemoteAttack = function(id, target, type)
 	{
-		if (charactersList[id])
-		{
-			charactersList[id].fire(target);
-		}
+        console.log("eurecaClient.exports.castRemoteAttack");
+		if (charactersList[id]) charactersList[id].fire(target,type);
 	}
 	
 	eurecaClient.exports.spawnEnemy = function(i, x, y, r, g, b)
